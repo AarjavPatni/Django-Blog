8fa8a3e27aa6b91bf1b44fc1fdf069e5a5f2c49c
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-10 18:01:56 -0500

    Add Post model to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.register(Post)
+# What does this do?
+# The code registers the Post model with the admin site.
+# Registering means that the Post model will be visible on the admin site.
