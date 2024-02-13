72b25009bcccfc79b650384ac3f19d9aef8cf9ea
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-13 13:30:21 -0500

    Add post_detail view and URL pattern

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     # The domain name is ignored. Tells Django to go to the views.post_list view at the domain root.
     # The name is used to identify the view.
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    # When django encounters a URL like post/5/, it will call the post_detail view with 5 as pk's value.
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,19 @@ Views request information from the model and pass it to a template.
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
-def post_list(request):
+def post_list(request):  # This view lists all the posts
     me = User.objects.get(username="aarjav")
-    posts = Post.objects.filter(author=me).order_by(
-        "published_date"
-    )
+    posts = Post.objects.filter(author=me).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
     # Essentially saying "Take the request and return the render of the template blog/post_list.html"
     # Syntax of render function: render(request, template_name, extras)
     # extras is a dictionary of variables to pass to the template
+
+
+def post_detail(request, pk):  # This view displays a single post
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
