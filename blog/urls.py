cd4bcd55b742ba0975a18b106afcabe0d8b3c25d
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-13 14:59:55 -0500

    Add post editing functionality

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # The name is used to identify the view.
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     # When django encounters a URL like post/5/, it will call the post_detail view with 5 as pk's value.
-    path("post/new/", views.post_new, name="post_new")
+    path("post/new/", views.post_new, name="post_new"),
     # Syntax: path(route, view, name)
+    path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
 ]
