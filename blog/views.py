0df6ad4e9d6cf09695cdbc3e9382799243a23331
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-13 02:33:47 -0500

    Add post_list view to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+"""
+Views request information from the model and pass it to a template.
+"""
+
 from django.shortcuts import render
 
+
 # Create your views here.
+def post_list(request):
+    return render(request, "blog/post_list.html", {})
+    # Essentially saying "Take the request and return the render of the template blog/post_list.html"
+    # Syntax of render function: render(request, template_name, context)
