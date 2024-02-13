ac3c13733ba57580bf45b2e2905ca607179f1269
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-13 02:59:14 -0500

    Add published posts to post_list view

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,16 @@ Views request information from the model and pass it to a template.
 """
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/post_list.html", {"posts": posts})
     # Essentially saying "Take the request and return the render of the template blog/post_list.html"
-    # Syntax of render function: render(request, template_name, context)
+    # Syntax of render function: render(request, template_name, extras)
+    # extras is a dictionary of variables to pass to the template
