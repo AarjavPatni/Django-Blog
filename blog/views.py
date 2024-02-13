5ef274658223c9cb13a2d27f78bddfab8569c7b5
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-13 03:23:31 -0500

    Update blog post list template and add Bootstrap styling

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,14 @@ Views request information from the model and pass it to a template.
 """
 
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Post
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+    me = User.objects.get(username="aarjav")
+    posts = Post.objects.filter(author=me).order_by(
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
